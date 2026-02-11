eb8bf59f04933b42b1f92bcb63ebdfe05ef21024
jack-dark <decker3d@gmail.com>
2026-02-11 14:03:31 -0800

    feat: adds jwt logic and tests

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,51 @@
 import { hash, verify } from "argon2"
+import type { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import { UserNotAuthenticatedError } from "./api/errors.js";
 
-export const hashPassword = (password: string): Promise<string> => {
-  return hash(password)
+const TOKEN_ISSUER = "chirpy";
+
+export const hashPassword = (password: string) => {
+  return hash(password);
 }
 
-export const checkPasswordHash = (password: string, hash: string): Promise<boolean> => {
-  return verify(hash, password)
+export const checkPasswordHash = async (password: string, hash: string) => {
+  if (!password) return false;
+  try {
+    return await verify(hash, password);
+  } catch {
+    return false;
+  }
+}
+
+type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export const makeJWT = (userID: string, expiresIn: number, secret: string): string => {
+  const iat = Math.floor(Date.now() / 1000);
+  const payload: Payload = {
+    iss: 'chirpy',
+    sub: userID,
+    iat,
+    exp: iat + expiresIn
+  }
+  return jwt.sign(payload, secret);
+}
+
+export const validateJWT = (tokenString: string, secret: string): string => {
+  let decoded: Payload;
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch (e) {
+    throw new UserNotAuthenticatedError("Invalid token");
+  }
+
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UserNotAuthenticatedError("Invalid issuer");
+  }
+
+  if (!decoded.sub) {
+    throw new UserNotAuthenticatedError("No user ID in token");
+  }
+
+  return decoded.sub;
 }
